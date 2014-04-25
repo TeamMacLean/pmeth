526d4b8af5f3ca89d6b6e538b5660a65f81ce344
Edward Chalstrey <edwardchalstrey@gmail.com>
2014-04-25 15:51:51 +0100

    added license to RubyGems.org

--- pmeth.gemspec
+++ pmeth.gemspec
@@ -1,8 +1,8 @@
 #encoding: utf-8
 Gem::Specification.new do |s|
   s.name        = 'pmeth'
-  s.version     = '0.0.1'
-  s.date        = '2014-04-24'
+  s.version     = '0.0.2'
+  s.date        = '2014-04-25'
   s.summary     = "Methods for permutation reproduction"
   s.description = "Reproduction methods for genetic (and other iterative improvement) algorithms, being used to solve permutation problems, 
   where permutations are arrays of unique objects. github: https://github.com/edwardchalstrey1/pmeth"
@@ -11,5 +11,5 @@ Gem::Specification.new do |s|
   s.files       = ["lib/pmeth.rb"]
   s.homepage    =
     'http://rubygems.org/gems/pmeth'
-  s.license       = 'none'
+  s.license       = 'MIT'
 end
--- test/unit_test_pmeth.rb
+++ test/unit_test_pmeth.rb
@@ -1,5 +1,5 @@
 #encoding: utf-8
-require_relative 'lib/pmeth'
+require 'pmeth'
 require 'test/unit'
 
 class TestPMeth < Test::Unit::TestCase
@@ -8,17 +8,17 @@ class TestPMeth < Test::Unit::TestCase
 	T2 = TEST_ARRAY[0..-2] # 19
 
 	def test_division
-		ax = PMeth::division(TEST_ARRAY)
-		bx = PMeth::division(T2)
+		ax = PMeth.division(TEST_ARRAY)
+		bx = PMeth.division(T2)
 		assert(ax==2||ax==1||ax==4||ax==5||ax==10||bx==20) 
 		assert(bx==9||bx=6||bx==3||bx==2||bx==1||bx==19)
 	end
 
 	def test_recombination
 		ts = TEST_ARRAY.shuffle
-		child = PMeth::recombine(TEST_ARRAY, ts)
+		child = PMeth.recombine(TEST_ARRAY, ts)
 		t2s = T2.shuffle
-		child2 = PMeth::recombine(T2, t2s)
+		child2 = PMeth.recombine(T2, t2s)
 
 		assert(child.uniq == child, 'Child not unique')
 		assert(child != TEST_ARRAY, 'Child same as test array')
@@ -32,7 +32,7 @@ class TestPMeth < Test::Unit::TestCase
 	end
 
 	def test_mutate
-		mutant = PMeth::mutate(TEST_ARRAY)
+		mutant = PMeth.mutate(TEST_ARRAY)
 		assert(mutant.uniq == mutant)
 		assert(mutant != TEST_ARRAY, 'Mutant was the same as parent')
 		assert_kind_of(Array, mutant, 'Mutant not an array!')
@@ -40,7 +40,7 @@ class TestPMeth < Test::Unit::TestCase
 	end
 
 	def test_mini_mutate
-		mini_mutant = PMeth::mini_mutate(TEST_ARRAY)
+		mini_mutant = PMeth.mini_mutate(TEST_ARRAY)
 		assert(mini_mutant.uniq == mini_mutant)
 		assert(mini_mutant != TEST_ARRAY, "Mini-mutant same as non-mutant")
 		assert_kind_of(Array, mini_mutant)
